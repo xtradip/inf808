40786d4f6b009f1da050c26b95cd402309677036
xtradip <robert.byrka@outlook.com>
2021-08-31 15:03:20 -0400

    freq_cipher

--- substitution.py
+++ substitution.py
@@ -1,4 +1,5 @@
 #coding: utf-8
+import binascii
 
 def freq_text(file):
     '''prend un fichier en entree et retourne un dictionnaire avec la frequence pour chaque caracteres associe'''
@@ -10,6 +11,27 @@ def freq_text(file):
                 d[c] += 1
             else:
                 d[c] = 1
+    f.close()
     return sorted(d.items(), key= lambda item: item[1], reverse = True)
 
-print(freq_text("test.txt"))
+d = freq_text("text_hugo.txt")
+# print(d)
+
+def freq_cipher(file):
+    f = open(file,'rb')
+    d = dict()
+    with open(file, "rb") as binary_file:
+    # Seek a specific position in the file and read N bytes
+        binary_file.seek(0, 0)  # Go to beginning of the file
+        couple_bytes = binary_file.read(2)
+        while(couple_bytes):
+            if(couple_bytes in d):
+                d[couple_bytes] +=1
+            else:
+                d[couple_bytes] = 1
+            couple_bytes = binary_file.read(2)
+    f.close()
+    return sorted(d.items(), key= lambda item: item[1], reverse = True) 
+
+
+print(freq_cipher("cipher_text_1.bin"))
