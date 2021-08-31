61b013b43f2d15a7b4ab8516662f47a140a9cc08
xtradip <robert.byrka@outlook.com>
2021-08-31 00:45:13 -0400

    force brute termine

--- ceasar.py
+++ ceasar.py
@@ -7,7 +7,7 @@ def translation_char(c, k):
     elif(ord(c) >= 65 and ord(c) <= 90):#minuscule
         return chr(((ord(c) + k-65) % 26 + 65))
     else:
-        return 0
+        return c
 
 def encrypt(str, k):
     caesar = ""
@@ -20,11 +20,37 @@ def encrypt(str, k):
 def decrypt(str,k):
     return encrypt(str, (26-k)%26)
 
+
+def force_brute(str):
+    d = {}
+    x = 1
+    while(x != 26):
+        d[x] = decrypt(str,x)
+        x+=1
+    return d
+
+def check_force_brute(d, str):
+    # d est un dict avec la force brute, str est le message original
+    # fonction qui permet de checker si il existe le message original dans d 
+    # retourne True avec la cle qui permet de dechiffrer
+    for k_test in d:
+        # print(d[k_test])
+        if(d[k_test] == str):
+            return True, k_test
+    return False
+
 print(translation_char('A',26))
 k = 100
-a = encrypt("abcde", k)
-
-print(decrypt(a, k))
-
+m = "yes it was the right thing"
+print("###### Q2 ######")
+a = encrypt(m, k)
+print(a)
+#Q3
+print("###### Q3 ######")
+print(encrypt(decrypt("abcde", k),k))
+print("###### Q4 ######")
+d = force_brute(a)
+check, k_trouve = check_force_brute(d, m)
+print(decrypt(a,k_trouve))
 
 
