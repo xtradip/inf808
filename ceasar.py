36c897820ac8c0b277cf5994c62449b446c3bc3f
xtradip <robert.byrka@outlook.com>
2021-08-30 23:42:15 -0400

    ecnrypt fait

--- ceasar.py
+++ ceasar.py
@@ -2,11 +2,22 @@
 
 def translation_char(c, k):
     # t est la translation de char + k
-    if(ord(c) >= 97 and ord(c) <= 122):
-        t = ((ord(c) + k - 97) % 26 + 97)
-        
-        return chr(t)
+    if(ord(c) >= 97 and ord(c) <= 122): #majuscule
+        return chr(((ord(c) + k - 97) % 26 + 97))
+    elif(ord(c) >= 65 and ord(c) <= 90):#minuscule
+        return chr(((ord(c) + k-65) % 26 + 65))
     else:
         return 0
 
-print(translation_char('a',26))
+def encrypt(str, k):
+    caesar = ""
+    #encrypte le message en utilisant la translation de char de caesar
+    for c in str:
+        caesar += translation_char(c,k)
+    
+    return caesar
+
+print(translation_char('A',26))
+print(encrypt("abcde", 1))
+
+
